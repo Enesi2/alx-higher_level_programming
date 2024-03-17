919971de0ec648f85475c33cb0133e426b1d0595
Enesi2 <emmymy2@gmail.com>
2024-03-17 16:17:55 -0700

    model_city

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
+    for finstance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(finstance)
     session.commit()
